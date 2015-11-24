52b7bc57bf6fc4c480a7346c2d07cfe9d42bebc7
Alexander Lewitt <alewitt2@gmail.com>
2015-11-24 14:05:01 -0500

    use proper method

--- classes/swift.php
+++ classes/swift.php
@@ -409,7 +409,7 @@ class Swift extends Swift_Plugin_Base {
 		$password = $creds['password'];
 		$projectId = $creds['projectId'];
 		
-		$objectUrl = $openstack->objectStoreV1()
+		$objectUrl = swift_get_client()
                     ->getContainer($myBucket)
                     ->getObject($myKey)
                     ->getPublicUri();
